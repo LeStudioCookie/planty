c6638f85373d0d5a792e5be66b946975b434bd41
LeStudioCookie <lestudiocookie@gmail.com>
2023-03-07 16:53:32 +0100

    Explication aux fonctions utilisés

--- functions.php
+++ functions.php
@@ -4,16 +4,14 @@
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
-    // Chargement du style.css du thème parent Twenty Twenty
+    /*Chargement du style.css du thème parent Twenty Twenty*/
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
 
 
-/**
- * Override loop template and show quantities next to add to cart buttons
- */
+/*Remplacer le modèle de boucle et afficher les quantités à côté des boutons d'ajout au panier*/
 add_filter( 'woocommerce_loop_add_to_cart_link', 'quantity_inputs_for_woocommerce_loop_add_to_cart_link', 10, 2 );
 function quantity_inputs_for_woocommerce_loop_add_to_cart_link( $html, $product ) {
 	$html = '<form action="' . esc_url( $product->add_to_cart_url() ) . '" class="cart" method="post" enctype="multipart/form-data">';
@@ -23,5 +21,5 @@ function quantity_inputs_for_woocommerce_loop_add_to_cart_link( $html, $product 
     $html .= '</form>';
 	return $html;
 }
-
+/*Suppression du H2*/
 remove_action( 'woocommerce_shop_loop_item_title','woocommerce_template_loop_product_title', 10 );
